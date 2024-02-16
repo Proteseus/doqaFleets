5cdbe131288424b6f6434cc1395c6fc604c2f456
Proteseus <lewiwui@gmail.com>
2024-02-16 12:49:29 +0300

    routing view

--- doqa/doqa_core/urls.py
+++ doqa/doqa_core/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import MarkersMapView, create_vehicle, create_employee, create_maintenance, create_trip, create_inventory, employee_list
+from .views import map, showroute, create_vehicle, create_employee, create_maintenance, create_trip, create_inventory, employee_list
 
 urlpatterns = [
-    path('map/', MarkersMapView.as_view(), name='map'),
-    path('register_vehicle/', create_vehicle, name='register_vehicle'),
+    path('map/', map, name='map'),
+    path('<str:lat1>,<str:long1>,<str:lat2>,<str:long2>', showroute, name='showroute'),
+    path('register_vehicle/', create_vehicle, name='register_vehicle'), 
     path('register_employee/', create_employee, name='register_employee'),
     path('register_maintenance/', create_maintenance, name='register_maintenance'),
     path('register_trip/', create_trip, name='register_trip'),
--- doqa/doqa_core/views.py
+++ doqa/doqa_core/views.py
@@ -2,18 +2,33 @@ from logging import log
 from django.db.models.fields import Empty
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+
+import folium
+
 from .forms import VehicleForm, EmployeeForm, MaintenanceForm, TripsForm, InventoryForm
 from .models import Vehicle, Employee, Maintenance, Trips, Inventory
+from .getroute import get_route
 
-from django.views.generic.base import (
-    TemplateView,
-)
-
-class MarkersMapView(TemplateView):
-    template_name = "map.html"
 
 def map(request):
-    return render(request, 'map.html')
+    map_token = 'pk.eyJ1IjoiaG9ycmlibGVib2IxMSIsImEiOiJjbHNvZm42ZWEwYmViMmprNTRpaThvZ2ZhIn0.ROEbyxFl7m7I-7KNof8kMQ'
+    # return render(request, 'map.html', {'mapbox_access_token': map_token})
+    return render(request, 'showmap.html')
+
+def showroute(request,lat1,long1,lat2,long2):
+    figure = folium.Figure()
+    lat1,long1,lat2,long2=float(lat1),float(long1),float(lat2),float(long2)
+    route=get_route(long1,lat1,long2,lat2)
+    m = folium.Map(location=[(route['start_point'][0]),
+                                 (route['start_point'][1])], 
+                       zoom_start=10)
+    m.add_to(figure)
+    folium.PolyLine(route['route'],weight=8,color='blue',opacity=0.6).add_to(m)
+    folium.Marker(location=route['start_point'],icon=folium.Icon(icon='play', color='green')).add_to(m)
+    folium.Marker(location=route['end_point'],icon=folium.Icon(icon='stop', color='red')).add_to(m)
+    figure.render()
+    context={'map':figure}
+    return render(request,'showroute.html',context)
 
 @login_required
 def create_vehicle(request):
