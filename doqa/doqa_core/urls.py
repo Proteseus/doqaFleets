0ce003237c3897693f86d72b6bd394e760ce471c
Proteseus <lewiwui@gmail.com>
2024-02-19 19:23:02 +0300

    list views

--- doqa/doqa_core/urls.py
+++ doqa/doqa_core/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import map, showroute, create_vehicle, create_employee, create_maintenance, create_trip, create_inventory, employee_list
+from .views import map, showroute, create_vehicle, create_employee, create_maintenance, create_trip, create_inventory, employee_list, vehicle_list, trips_list
 
 urlpatterns = [
     path('map/', map, name='map'),
@@ -7,10 +7,17 @@ urlpatterns = [
     #path('showroute/<str:lat1>,<str:long1>,<str:lat2>,<str:long2>/', showroute, name='showroute_with_params'),
     # path('', showroute, name='showroute'),
     path('routed/<str:lat1>,<str:long1>,<str:lat2>,<str:long2>', showroute, name='showroute_updated'),
+
     path('register_vehicle/', create_vehicle, name='register_vehicle'), 
+    path('vehicle_list/', vehicle_list, name='vehicle_list'),
+    
     path('register_employee/', create_employee, name='register_employee'),
+    path('employee_list/', employee_list, name='employee_list'),
+    
     path('register_maintenance/', create_maintenance, name='register_maintenance'),
+    
     path('register_trip/', create_trip, name='register_trip'),
+    path('trips_list/', trips_list, name='trips_list'),
+
     path('register_inventory/', create_inventory, name='register_inventory'),
-    path('employee-list/', employee_list, name='employee_list'),
 ]
