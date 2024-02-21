5a13e79cf2be56bb3bde0a434d71a6602ff62a86
Proteseus <lewiwui@gmail.com>
2024-02-21 22:12:46 +0300

    django-extensions django-widget-tweaks added

--- doqa/doqa/settings.py
+++ doqa/doqa/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.gis',
+    'widget_tweaks',
+    'django_extensions',
     'doqa_core'
 ]
 
