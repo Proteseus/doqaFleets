25199c16fc319a09fabc64625dd0ebd13c6508be
Proteseus <lewiwui@gmail.com>
2024-02-23 22:54:50 +0300

    vehicle image support

--- doqa/doqa_core/models.py
+++ doqa/doqa_core/models.py
@@ -6,6 +6,7 @@ from django.contrib.auth.models import User
 class Vehicle(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     registration_number = models.CharField(max_length=20)
+    image = models.ImageField(upload_to ='uploads/vehicles/', null=True)
     make = models.CharField(max_length=50)
     model = models.CharField(max_length=50)
     year = models.IntegerField()
