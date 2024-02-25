7ee2cd105e9c92ccdeec7355c5739f1d9ec30114
Proteseus <lewiwui@gmail.com>
2024-02-25 15:25:14 +0300

    reverse geocode api

--- doqa/doqa_core/utils.py
+++ doqa/doqa_core/utils.py
@@ -26,4 +26,55 @@ def find_coordinates(location):
         print(f"Error: {response.status_code}, {response.text}")
         return {'status': response.status_code, 'message': response.text}
 
-find_coordinates('vibes hotel')
+def find_address(location):
+    url = "https://revgeocode.search.hereapi.com/v1/revgeocode"
+    params = {
+        'in': f'circle:{location};r=2000',
+        'types': 'city',
+        'lang': 'en-US',
+        'limit': 1,
+        'apiKey': 'mS2xA92lRv89XGpbQlxp5znBgCHdi6u-zOf-Ttecheo'
+    }
+
+    response = requests.get(url, params=params)
+
+    if response.status_code == 200:
+        data = response.json()
+        if len(data['items']) >= 1:
+            result = {'result': [data['items'][0]['title'], data['items'][0]['position']['lat'], data['items'][0]['position']['lng']]}
+            print(result)
+            return result
+        else:
+            print(f'not found for {location}')
+            return {'result': 'Not found'}
+    else:
+        print(f"Error: {response.status_code}, {response.text}")
+        return {'status': response.status_code, 'message': response.text}
+
+
+def find_city(location):
+    url = "https://geocode.search.hereapi.com/v1/geocode"
+    params = {
+        'q': location,
+        'in': 'countryCode:ETH',
+        'lang': 'en-US',
+        'limit': 1,
+        'apiKey': 'mS2xA92lRv89XGpbQlxp5znBgCHdi6u-zOf-Ttecheo'
+    }
+
+    response = requests.get(url, params=params)
+
+    if response.status_code == 200:
+        data = response.json()
+        if len(data['items']) >= 1:
+            result = {'result': [data['items'][0]['title'], data['items'][0]['position']['lat'], data['items'][0]['position']['lng']]}
+            print(result)
+            return result
+        else:
+            print('not found')
+            return {'result': 'Not found'}
+    else:
+        print(f"Error: {response.status_code}, {response.text}")
+        return {'status': response.status_code, 'message': response.text}
+
+find_address('50.1158056,8.6786751')
